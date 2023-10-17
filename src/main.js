9519e96438e1dc7cbec996f713cbfd125863ba9d
Raul Yesid Rojas Ballesteros <131273447+YesidRojas06@users.noreply.github.com>
2023-10-17 14:49:16 -0500

    Falta instalar quasar y v-router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+import {Quasar, Dialog} from 'quasar'
+import { createPinia} from 'pinia'
+// import './style.css'
+import App from './App.vue'
+import {router} from "./routes/routes.js"
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(Quasar, {
+    plugins:{Dialog}
+})
+app.use(router)
+app.use(pinia)
+app.mount('#app')
