93c02facde1a5b10fc019422dfa992d087410d5f
Jorge Luis <65985738+Jorge1825@users.noreply.github.com>
2023-12-01 16:49:20 -0500

    Cambios2

--- src/components/ventas.vue
+++ src/components/ventas.vue
@@ -6,20 +6,10 @@
     </div>
     <div class="r">
       <button type="button" class="  button " style="margin:0 auto;" data-bs-toggle="modal"
-        data-bs-target="#staticBackdrop" @click="abrir()"> <span class="button__text">Nueva Venta</span> <span
-          class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2"
-            stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg">
-            <line y2="19" y1="5" x2="12" x1="12"></line>
-            <line y2="12" y1="12" x2="19" x1="5"></line>
-          </svg></span>
+        data-bs-target="#staticBackdrop" @click="abrir()"> <span class="button__text">Nueva Venta</span>
       </button>
       <button type="button" class="  button " style="margin:0 auto;" data-bs-toggle="modal" @click="abrirContinuarVenta()"
-        data-bs-target="#staticBackdrop"> <span class="button__text">Continuar Venta</span> <span
-          class="button__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" viewBox="0 0 24 24" stroke-width="2"
-            stroke-linejoin="round" stroke-linecap="round" stroke="currentColor" height="24" fill="none" class="svg">
-            <line y2="19" y1="5" x2="12" x1="12"></line>
-            <line y2="12" y1="12" x2="19" x1="5"></line>
-          </svg></span>
+        data-bs-target="#staticBackdrop"> <span class="button__text">Continuar Venta</span> 
       </button>
     </div>
 
